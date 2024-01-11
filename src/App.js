1fe57752959ca815de47154530f1a0e4bfe06cd8
KonstPass <passadis@gmail.com>
2024-01-11 22:18:16 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -14,14 +14,8 @@ import Footer from "./components/Footer";
 
 const App = () => {
     const { profile } = config;
-    
-    const specificRepos = useMemo(() => [
-        'python-aivision', 
-        'learning-aid', 
-        'CosmosDB-ADUsers', 
-        'azure-keyvault-pinlogin', 
-        'react-customidentitydb'
-    ], []);
+
+    const specificRepos = useMemo(() => ['python-aivision', 'learning-aid', 'CosmosDB-ADUsers', 'AzureMonitorCommunity', 'react-customidentitydb'], []);
 
     const [specificReposData, setSpecificReposData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -40,35 +34,52 @@ const App = () => {
             setLoading(false);
         });
     }, [profile.username, specificRepos]);
+    
 
     return (
         <div className="min-h-screen">
             <div className="p-4 md:p-10 flex flex-col lg:flex-row gap-6">
                 <div className="w-full lg:w-2/6 space-y-6">
                     <ChangeTheme/>
+
                     <Resume/>
+
                     <Contact/>
+
                     <TechStack/>
-                    <Experience title={"Experience"} data={profile.experience}/>
-                    <Experience title={"Education"} data={profile.eduction}/>
+
+                    <Experience
+                        title={"Experience"}
+                        data={profile.experience}
+                    />
+
+                    <Experience
+                        title={"Education"}
+                        data={profile.eduction}
+                    />
                 </div>
+
                 <div className="w-full lg:w-4/6 space-y-6">
                     <ProjectsContainer>
                         <div className="flex items-center justify-between">
                             <CardTitle>My Projects</CardTitle>
+
                         </div>
+
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                            {loading ? (
-                                <SkeletonProject count={specificRepos.length} />
-                            ) : (
-                                specificReposData.map((repo, index) => (
-                                    <Project key={index} data={repo} />
-                                ))
-                            )}
-                        </div>
+                        {loading ? (
+                            <SkeletonProject count={specificRepos.length} />
+                        ) : (
+                            specificReposData.map((repo, index) => (
+                                <Project key={index} data={repo} />
+                            ))
+                        )}
+                    </div>
                     </ProjectsContainer>
+
                     <ProjectsContainer>
                         <CardTitle>Workshops</CardTitle>
+
                         <div className="space-y-6">
                             {profile.tryProjects.map((item, index) => (
                                 <TryProjects key={index} data={item}/>
@@ -77,6 +88,7 @@ const App = () => {
                     </ProjectsContainer>
                 </div>
             </div>
+
             <Footer/>
         </div>
     );
