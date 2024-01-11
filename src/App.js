bdaf51cf2ce4560ccf0534e49bf0a70f198a81bd
KonstPass <passadis@gmail.com>
2024-01-11 22:10:04 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import config from "./config";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import TechStack from "./components/TechStack";
+import ChangeTheme from "./components/ChangeTheme";
+import Experience from "./components/Experience";
+import {CardTitle, ProjectsContainer} from "./components/utils";
+import Project from "./components/Project";
+import TryProjects from "./components/TryProjects";
+import {useFetchData} from "./hooks";
 import React, { useEffect, useState, useMemo } from 'react';
-import config from './config'; // Import the 'config' object from the appropriate file
+import {SkeletonProject} from "./components/SkeletonProject";
+import Footer from "./components/Footer";
 
 const App = () => {
     const { profile } = config;
-
-    const specificRepos = useMemo(() => ['python-aivision', 'learning-aid', 'CosmosDB-ADUsers', 'azure-keyvault-pinlogin', 'react-customidentitydb'], []);
+    
+    const specificRepos = useMemo(() => [
+        'python-aivision', 
+        'learning-aid', 
+        'CosmosDB-ADUsers', 
+        'azure-keyvault-pinlogin', 
+        'react-customidentitydb'
+    ], []);
 
     const [specificReposData, setSpecificReposData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -23,52 +40,35 @@ const App = () => {
             setLoading(false);
         });
     }, [profile.username, specificRepos]);
-    
 
     return (
         <div className="min-h-screen">
             <div className="p-4 md:p-10 flex flex-col lg:flex-row gap-6">
                 <div className="w-full lg:w-2/6 space-y-6">
                     <ChangeTheme/>
-
                     <Resume/>
-
                     <Contact/>
-
                     <TechStack/>
-
-                    <Experience
-                        title={"Experience"}
-                        data={profile.experience}
-                    />
-
-                    <Experience
-                        title={"Education"}
-                        data={profile.eduction}
-                    />
+                    <Experience title={"Experience"} data={profile.experience}/>
+                    <Experience title={"Education"} data={profile.education}/>
                 </div>
-
                 <div className="w-full lg:w-4/6 space-y-6">
                     <ProjectsContainer>
                         <div className="flex items-center justify-between">
                             <CardTitle>My Projects</CardTitle>
-
                         </div>
-
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                        {loading ? (
-                            <SkeletonProject count={specificRepos.length} />
-                        ) : (
-                            specificReposData.map((repo, index) => (
-                                <Project key={index} data={repo} />
-                            ))
-                        )}
-                    </div>
+                            {loading ? (
+                                <SkeletonProject count={specificRepos.length} />
+                            ) : (
+                                specificReposData.map((repo, index) => (
+                                    <Project key={index} data={repo} />
+                                ))
+                            )}
+                        </div>
                     </ProjectsContainer>
-
                     <ProjectsContainer>
                         <CardTitle>Workshops</CardTitle>
-
                         <div className="space-y-6">
                             {profile.tryProjects.map((item, index) => (
                                 <TryProjects key={index} data={item}/>
@@ -77,7 +77,6 @@ const App = () => {
                     </ProjectsContainer>
                 </div>
             </div>
-
             <Footer/>
         </div>
     );
